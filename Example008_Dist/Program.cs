51b50cfb7ff51c0b2d3ff2475d1413a64ecb56fa
VladislavRazvodov <g-man-111@ya.ru>
2022-08-29 14:18:42 +0300

    add Example008_Dist

---
+++ Example008_Dist/Program.cs
@@ -0,0 +1,25 @@
+﻿int count = 0,
+    friend = 2;
+
+double  distance = 10000,
+        firstFriendSpeed = 1,
+        secondFriendSpeed = 2,
+        dogSpeed = 5,
+        time = 0;
+
+while (distance > 10){
+    if (friend == 1){
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else{
+        time = distance / (secondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+
+    distance = distance - (firstFriendSpeed + secondFriendSpeed)*time;
+    count++;
+}
+
+
+Console.WriteLine($"Собака развернется {count} раз.");
