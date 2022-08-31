fb568de65f573992787ee9b08073dbb77688c1fb
VladislavRazvodov <g-man-111@ya.ru>
2022-08-31 22:35:40 +0300

    v1

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = {12,31,17,33,15,35,44,11};
+
+int n = array.Length;
+int find = 33;
+
+int index = 0;
+
+while (index < n ){
+    if (array[index]== find){
+        Console.WriteLine(index);
+    }
+    index++;
+}
